4de2c0ef27c061bdc64655861e136373231ca899
Rodion Tkachenko <tkachenko_rp@mail.ru>
2024-01-31 11:51:01 +0700

    init

---
+++ src/main/java/ru/tkachenko/springhostel/SpringHostelApplication.java
@@ -0,0 +1,13 @@
+package ru.tkachenko.springhostel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHostelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHostelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/tkachenko/springhostel/SpringHostelApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tkachenko.springhostel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHostelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
